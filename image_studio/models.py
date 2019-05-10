943494182aefd766c3f537c5778d60c9feac7f5b
kipkemoimayor <collinskipkemoi24@gmail.com>
2019-05-10 14:15:33 +0300

    Created models for location and category

--- image_studio/models.py
+++ image_studio/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    name=models.CharField(max_length=30)
+
+class Location(models.Model):
+    location=models.CharField(max_length=30)
+
+class Category(models.Model):
+    Category=models.CharField(max_length=30)
--- static/js/main.js
+++ static/js/main.js
@@ -2,4 +2,10 @@ $(document).ready(function(){
   $("#test").click(function(){
     alert("success")
   })
+  $("#clickSearch").click(function(){
+    $("#search").fadeIn(2000)
+    $("#search").show()
+
+
+  })
 })
