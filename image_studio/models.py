e7d6e5ce58381757b768374eda0f664620c75515
kipkemoimayor <collinskipkemoi24@gmail.com>
2019-05-10 16:01:03 +0300

    Created a admin panel and regitered all the models

--- image_studio/models.py
+++ image_studio/models.py
@@ -1,11 +1,29 @@
 from django.db import models
 
 # Create your models here.
-class Image(models.Model):
-    name=models.CharField(max_length=30)
 
 class Location(models.Model):
     location=models.CharField(max_length=30)
 
+    def __str__(self):
+        return self.location
+
 class Category(models.Model):
     Category=models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.Category
+
+class Image(models.Model):
+    name=models.CharField(max_length=30)
+    decription=models.CharField(max_length=200)
+    locate=models.ForeignKey(Location)
+    categ=models.ForeignKey(Category)
+    image=models.ImageField(upload_to='gallery/',blank=True)
+    upload_time=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering=['name']
