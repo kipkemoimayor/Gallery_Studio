e06c6297a41a2d3085fa5cbe8c298c8ff1cf906e
kipkemoimayor <collinskipkemoi24@gmail.com>
2019-05-10 07:20:13 +0300

    Installed all the packages for django

--- apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class GalleryStudioConfig(AppConfig):
-    name = 'gallery_studio'
--- gallery_studio/wsgi.py
+++ gallery_studio/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for image_studio project.
+WSGI config for gallery_studio project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "image_studio.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gallery_studio.settings")
 
 application = get_wsgi_application()
